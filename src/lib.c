f5381ef7c6ac1817c89d8f003ef2793d5b953391
Pedro Weber <pedroweberm@gmail.com>
2019-10-01 22:21:30 -0300

    init function and thread creation

--- src/lib.c
+++ src/lib.c
@@ -5,40 +5,102 @@
 #include "../include/cthread.h"
 #include "../include/cdata.h"
 
+bool initialized = false;
+
+int currentTid = 0;
+
+PFILA2 filaAptos;
+PFILA2 filaBloqueados;
+PFILA2 filaExecutando;
+
+int init()
+{
+    if (!initialized)
+    {
+        CreateFila2(filaAptos);
+        CreateFila2(filaBloqueados);
+        CreateFila2(filaExecutando);
+
+
+
+        initialized = true;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
+    init()
+
+    TCB_t newThread;
+    TCB_t* newThreadP = &newThread;
+
+    ucontext_t newContext;
+    ucontext_t* newContextP;
+
+    getcontext(newContext);
+    makecontext(newContext, start, arg);
+
+    newThread->tid = ++currentTid;
+    newThread->state = 0;
+    newThread->prio = 0;
+    newThread->context = newContext;
+
+    AppendFila2(filaAptos, newThread);
+
 	return -1;
 }
 
 int cyield(void) {
+
+    init()
+
 	return -1;
 }
 
 int cjoin(int tid) {
+
+    init()
+
 	return -1;
 }
 
 int csem_init(csem_t *sem, int count) {
+
+    init()
+
 	return -1;
 }
 
 int cwait(csem_t *sem) {
+
+    init()
+
 	return -1;
 }
 
 int csignal(csem_t *sem) {
+
+    init()
+
 	return -1;
 }
 
 int cidentify (char *name, int size) {
-	strncpy (name, "Demetrio Boeira - xxxxxx\nGustavo Lorentz - 287681\nPedro Weber - 287678\n", size);
+
+    init()
+
+	strncpy (name, "Demetrio Boeira - 297693\nGustavo Lorentz - 287681\nPedro Weber - 287678\n", size);
 	return 0;
 }
 
 int main() {
-    char name[74] = "";
+    char name[71] = "";
     char *namep = &name;
-    int size = 74;
+    int size = 71;
     cidentify(namep, size);
     printf("%s", name);
     return 0;

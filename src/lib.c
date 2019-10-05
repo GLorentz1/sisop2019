0e7964a51fb28ca606264d019f3f84f6882b311f
Pedro Weber <pedroweberm@gmail.com>
2019-10-05 00:13:07 -0300

    ccreate e init

--- src/lib.c
+++ src/lib.c
@@ -5,25 +5,53 @@
 #include "../include/cthread.h"
 #include "../include/cdata.h"
 
+#define	PROCST_APTO	0		/* Processo em estado apto a ser escalonado */
+#define	PROCST_EXEC	1		/* Processo em estado de execução */
+#define	PROCST_BLOQ	2		/* Processo em estado bloqueado */
+#define	PROCST_TERMINO	3	/* Processo em estado de terminado */
+
 bool initialized = false;
 
 int currentTid = 0;
 
-PFILA2 filaAptos;
-PFILA2 filaBloqueados;
-PFILA2 filaExecutando;
+ucontext_t contextEscalonador;
+
+FILA2 filaAptos;
+FILA2 filaBloqueado;
+FILA2 filaExecutando;
+
+PFILA2 pfilaAptos;
+PFILA2 pfilaBloqueados;
+PFILA2 pfilaExecutando;
 
 int init()
 {
     if (!initialized)
     {
-        CreateFila2(filaAptos);
-        CreateFila2(filaBloqueados);
-        CreateFila2(filaExecutando);
+        getcontext(&contextEscalonador);
 
+        contextEscalonador.uc_link = NULL;
+        contextEscalonador.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
+        contextEscalonador.uc_stack.ss_size = SIGSTKSZ;
+        contextEscalonador.uc_stack.ss_flags = 0;
 
+        makecontext(&contextEscalonador, (void (*)(void))escalonador, 0);
+
+        pfilaAptos = &filaAptos;
+        pfilaBloqueado = &filaBloqueado;
+        pfilaExecutando = &filaExecutando;
+
+        if ( CreateFila2(pfilaAptos) != 0)
+            exit(-1);
+
+        if ( CreateFila2(pfilaBloqueados) != 0)
+            exit(-1);
+
+        if ( CreateFila2(pfilaExecutando) != 0)
+            exit(-1);
 
         initialized = true;
+
         return 0;
     }
     else
@@ -32,22 +60,31 @@ int init()
     }
 }
 
+void escalonador()
+{
+
+
+}
+
 int ccreate (void* (*start)(void*), void *arg, int prio) {
     init()
 
-    TCB_t newThread;
-    TCB_t* newThreadP = &newThread;
+    TCB_t *newThread = malloc(sizeof(TCB_t))
 
     ucontext_t newContext;
     ucontext_t* newContextP;
 
-    getcontext(newContext);
-    makecontext(newContext, start, arg);
-
     newThread->tid = ++currentTid;
-    newThread->state = 0;
+    newThread->state = PROCST_APTO;
     newThread->prio = 0;
-    newThread->context = newContext;
+    getcontext(&(newThread->context));
+
+    newThread->context.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
+    newThread->context.uc_stack.ss_size = SIGSTKSZ;
+    newThread->context.uc_stack.ss_flags = 0;
+    newThread->context.uc_link = &escalonador
+
+    makecontext(&(newThread->context), (void (*)(void))start, 1, arg);
 
     AppendFila2(filaAptos, newThread);
 
@@ -56,42 +93,44 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
 
 int cyield(void) {
 
-    init()
+    init();
+
+
 
 	return -1;
 }
 
 int cjoin(int tid) {
 
-    init()
+    init();
 
 	return -1;
 }
 
 int csem_init(csem_t *sem, int count) {
 
-    init()
+    init();
 
 	return -1;
 }
 
 int cwait(csem_t *sem) {
 
-    init()
+    init();
 
 	return -1;
 }
 
 int csignal(csem_t *sem) {
 
-    init()
+    init();
 
 	return -1;
 }
 
 int cidentify (char *name, int size) {
 
-    init()
+    init();
 
 	strncpy (name, "Demetrio Boeira - 297693\nGustavo Lorentz - 287681\nPedro Weber - 287678\n", size);
 	return 0;
